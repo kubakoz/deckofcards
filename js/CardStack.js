4bc9339ca1fd9456be56a53935e9241d94d9f7ec
kubakoz <jakubkozlowski@hotmail.com>
2020-05-13 15:16:31 -0400

    added CardStack

---
+++ js/CardStack.js
@@ -0,0 +1,80 @@
+class CardStack {
+
+  constructor() {
+
+    this.cards = [];
+  }
+
+  addStack(cardStack) {
+    this.cards = this.cards.concat(cardStack.cards);
+    return this;
+  }
+
+  shuffle(times) {
+
+    if (!times) {
+      times = 1
+    }
+
+    while (times-- > 0) {
+
+      for (let i = 0; i < this.cards.length; i++) {
+
+        let randomNumber = Math.floor(Math.random() * this.cards.length);
+        let placeholder = this.cards[i];
+
+        this.cards[i] = this.cards[randomNumber];
+        this.cards[randomNumber] = placeholder;
+
+      }
+
+    }
+
+  }
+
+  addCard(card) {
+    this.cards.push(card);
+    return this;
+  }
+
+  removeCard(card) {
+    return this.cards.pop();
+    
+  }
+
+  giveCard(toStack) {
+
+    toStack.cards.push(this.cards.pop());
+    return this;
+
+  }
+  
+	giveSpecificCard(card, toStack){
+  
+  	const index = this.cards.indexOf(card);
+    
+    if (index > -1) {
+      this.cards.splice(index, 1);
+    }
+    
+  	toStack.cards.push(card);
+  }
+  
+  giveCardByType(rank, suit, toStack){
+  	
+      
+    for(let i = 0; i < this.cards.length; i++){
+      
+      console.log(this.cards[i].suit + "|" + suit)
+    	if(this.cards[i].suit == suit && this.cards[i].rank == rank){
+      		
+          
+      	 this.giveSpecificCard(this.cards[i], toStack);         
+      }
+      
+    }
+  
+  }
+
+
+}
